06e16dbee20a3323b14e9e6be12718a821090193
NormalTable5801 <61955835+NormalTable5801@users.noreply.github.com>
2020-06-25 10:51:02 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ForEachTest
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("确定要尝试吗？");
+        Console.ReadLine();
+        Console.WriteLine("祝你好运");
+        Console.ReadLine();
+    for (; ; )
+        {
+            System.Diagnostics.Process.Start("1.exe");
+            Console.WriteLine("去死吧");
+        }
+    }
+}
